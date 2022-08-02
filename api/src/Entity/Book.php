2b09232f3eb3727464c36fab16b4b4869fcc9570
msusana <mariasusanarodrigues@hotmail.com>
2022-08-02 12:53:52 +0000

    server

---
+++ api/src/Entity/Book.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+
+
+#[ORM\Entity(repositoryClass: BookRepository::class)]
+#[ApiResource]
+class Book
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column()]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $author = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $datePublished = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDatePublished(): ?\DateTimeInterface
+    {
+        return $this->datePublished;
+    }
+
+    public function setDatePublished(?\DateTimeInterface $datePublished): self
+    {
+        $this->datePublished = $datePublished;
+
+        return $this;
+    }
+}
---
+++ api/tests/Api/BooksTest.php
@@ -0,0 +1,57 @@
+<?php namespace App\Tests;
+
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use App\Entity\Book;
+
+class BooksTest extends ApiTestCase
+{
+    public function testGetCollection(): void
+    {
+        // The client implements Symfony HttpClient's `HttpClientInterface`, and the response `ResponseInterface`
+        $response = static::createClient()->request('GET', 'https://cda2-devops-gael.simplon-roanne.com/books');
+
+        $this->assertResponseIsSuccessful();
+        // Asserts that the returned content type is JSON-LD (the default)
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+
+        // Asserts that the returned JSON is a superset of this one
+        $this->assertJsonContains([
+            '@context' => '/contexts/Book',
+            '@id' => '/books',
+            '@type' => 'hydra:Collection',
+            'hydra:view' => [
+                '@id' => '/books?page=1',
+                '@type' => 'hydra:PartialCollectionView',
+                'hydra:first' => '/books?page=1',
+            ],
+        ]);
+
+        // Because test fixtures are automatically loaded between each test, you can assert on them
+        $this->assertCount(30, $response->toArray()['hydra:member']);
+
+        // Asserts that the returned JSON is validated by the JSON Schema generated for this resource by API Platform
+        // This generated JSON Schema is also used in the OpenAPI spec!
+        $this->assertMatchesResourceCollectionJsonSchema(Book::class);
+    }
+
+    public function testCreateBook(): void
+    {
+        $response = static::createClient()->request('POST', 'https://cda2-devops-gael.simplon-roanne.com/books', ['json' => [
+            'name' => 'The Handmaid\'s Tale',
+            'author' => 'Margaret Atwood',
+            'datePublished' => '1985-07-31T00:00:00+00:00',
+        ]]);
+
+        $this->assertResponseStatusCodeSame(201);
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+        $this->assertJsonContains([
+            '@context' => '/contexts/Book',
+            '@type' => 'Book',
+            'name' => 'The Handmaid\'s Tale',
+            'author' => 'Margaret Atwood',
+            'datePublished' => '1985-07-31T00:00:00+00:00',
+        ]);
+        $this->assertMatchesRegularExpression('~^/books/\d+$~', $response->toArray()['@id']);
+        $this->assertMatchesResourceItemJsonSchema(Book::class);
+    }
+}
