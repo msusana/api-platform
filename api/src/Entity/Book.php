fb976ffdfa23b9de3d389ef51b519ec031a2bc54
unknown <susymariarodri@gmail.com>
2022-08-03 11:44:41 +0200

    server_18

--- api/src/Entity/Book.php
+++ api/src/Entity/Book.php
@@ -2,11 +2,10 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\BookRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use ApiPlatform\Metadata\ApiResource;
-
 
 #[ORM\Entity(repositoryClass: BookRepository::class)]
 #[ApiResource]
